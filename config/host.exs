24f8e0313c0cea420ce171bc7d29e45ac0ea27ae
Tyler Hunt <tyler@tylerhunt.com>
2021-04-21 10:06:40 -0400

    Remove old default scene definition

--- config/host.exs
+++ config/host.exs
@@ -2,7 +2,6 @@ use Mix.Config
 
 config :alarm_clock, :viewport, %{
   name: :main_viewport,
-  # default_scene: {AlarmClock.Scene.Crosshair, nil},
   default_scene: {AlarmClock.Scene.Clock, nil},
   size: {128, 64},
   opts: [scale: 1.0],
